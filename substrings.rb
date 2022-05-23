6348359cb4fa083820d8a1164b6c9ab5f87973ab
Kropyls <Kropyls@gmail.com>
2022-05-23 15:43:27 -0700

    finished product. The reduce method seemed the cleanest way to meet the requirments.

---
+++ substrings.rb
@@ -0,0 +1,20 @@
+#finds substrings from given array of strings
+dict = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+#EXAMPLES:
+#substrings("below", dictionary)
+#{ "below" => 1, "low" => 1 }
+#substrings("Howdy partner, sit down! How's it going?", dictionary)
+#{ "down" => 1, "go" => 1, "going" => 1, "how" => 2, "howdy" => 1, "it" => 2, "i" => 3, "own" => 1, "part" => 1, "partner" => 1, "sit" => 1 }
+
+def substrings(string, dictionary)
+  string = string.downcase()
+  hash_of_substrings = dictionary.reduce(Hash.new(0)) do |acc, word|
+    if string.include?(word.downcase())
+
+      acc[word.downcase()] += string.scan(/(?=#{word.downcase()})/).count
+    end
+    acc
+  end
+end
+
+puts(substrings("Below", dict))
